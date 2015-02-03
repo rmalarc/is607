7bc87e268305544f71fa90b3fe1ceb7fdcd423e1
Mauricio A <malarcon@medivo.com>
2015-02-02 20:15:33 -0500

    added answers

--- assignment2/assignment2.sql
+++ assignment2/assignment2.sql
@@ -1,5 +1,47 @@
 ﻿/*
+Mauricio Alarcon
+IS606 - Assignment 2
+mauricio.alarcon_balan@spsmail.cuny.edu
+
+ANSWERS
+-------
 1. Which destination in the flights database is the furthest distance away?
+A. Honolulu
+
+2. What are the different numbers of engines in the planes table? For each number of engines, which aircraft have
+the most number of seats?
+A.
+	Engines	| Aircraft			| Seats
+	------------------------------------------------
+	 1	 Dehavilland OTTER DHC-3	 16
+	 2	 Boeing 777			 400
+	 3	 Airbus A330			 379
+	 4	 Boeing 747			 450
+
+3. What weather conditions are associated with New York City departure delays?
+A. The following weather conditions appear to be associated with longer departure delays in New York City:
+
+	-. Higher Temperatures
+	-. Higher Dewpoints
+	-. Greater Wind gust
+	-. Higher Precipitation
+	-. Lower Visibility
+	
+   One could argue that the first two are not necesarily the cause of more delays but more traffic as the 
+   summer comes with more air traffic.
+
+4. Are older planes more likely to be delayed?
+A. It does not appear to be so.
+
+5. (Self generated question) What aircraft travels the longest distance?
+A. The winner is the A330, with a maximum traveled distance of 4,983 miles
+
+6. EXTRA CREDIT QUESTION: List the two airports that are furthest apart
+A. Eastport Municipal Airport (EPM)- Maine and Eareckson As (SYA) - Alaska are the two furthest apart airports
+
+/*
+1. Which destination in the flights database is the furthest distance away?
+A. Honolulu
 */
 
 select dest,avg(distance) as avg_distance
@@ -11,8 +53,10 @@ order by avg_distance desc;
 
 
 --create index faa on airports(faa);
--- Extra credit question:  two airports that are furthest apart
 /*
+Q. Extra credit question:  two airports that are furthest apart
+A. Eastport Municipal Airport (EPM)- Maine and Eareckson As (SYA) - Alaska are the two furthest apart airports
+
 BEWARE OF BAD DATA: MYF airport's latitude and longitude is incorrect. 
 
 It appears as:
@@ -40,8 +84,16 @@ ORDER BY DISTANCE DESC;
 /*
 2. What are the different numbers of engines in the planes table? For each number of engines, which aircraft have
 the most number of seats?
+A.
+Engines	| Aircraft			| Seats
+------------------------------------------------
+1	 Dehavilland OTTER DHC-3	 16
+2	 Boeing 777			 400
+3	 Airbus A330			 379
+4	 Boeing 747			 450
+
 */
-select ps.engines, type, manufacturer, model, seats
+select ps.engines, manufacturer, model, seats
 from planes p
 inner join (
 	select engines,max(seats) max_seats 
@@ -52,6 +104,16 @@ order by engines,model, seats;
 
 /*
 3. What weather conditions are associated with New York City departure delays?
+A. The following weather conditions appear to be associated with longer departure delays in New York City:
+
+	-. Higher Temperatures
+	-. Higher Dewpoints
+	-. Greater Wind gust
+	-. Higher Precipitation
+	-. Lower Visibility
+	
+   One could argue that the first two are not necesarily the cause of more delays but more traffic as the 
+   summer comes with more air traffic.
 */
 --select d.*,w.temp,dewp,humid,wind_dir,wind_speed,wind_gust,precip,pressure,visib 
 select CASE 
@@ -91,6 +153,7 @@ order by dep_delay;
 
 /*
 4. Are older planes more likely to be delayed?
+A. It does not appear to be so.
 */
 
 select engine,f.year - p.year,avg(dep_delay),avg(arr_delay), count(*)
@@ -103,7 +166,6 @@ order by 1,2;
 Ask (and if possible answer) a question that also requires joining information from two or more tables in the
 flights database, and/or assumes that additional information can be collected in advance of answering your question.
 
-- What are the aircraft that travel the greatest distances?
 */
 
 select manufacturer,type,engine,model, avg(engines) engines, avg(seats) seats,max(distance) max_distance,avg(distance),count(*) as flights
